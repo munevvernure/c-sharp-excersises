08f18da550e46b339164ba0c0b0e7b0c299a3fd1
munevvernure <116505664+munevvernure@users.noreply.github.com>
2023-01-24 14:55:35 +0300

    Add files via upload

---
+++ circle.cs
@@ -0,0 +1,25 @@
+﻿// Write a C# Sharp program that takes the radius of a circle as input and calculate the perimeter and area of the circle.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpDataTypeExcersises
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double pi = 3.14;
+            Console.Write("Type the radius of the circle:");
+            int radius = Convert.ToInt32(Console.ReadLine());
+            double perimeter = 2 * pi * radius;
+            double area = pi * radius * radius;
+            Console.WriteLine("Perimeter of the circle is: " + perimeter);
+            Console.WriteLine("Area of the circle is: " + area);
+            Console.ReadLine();
+
+        }
+    }
+}
