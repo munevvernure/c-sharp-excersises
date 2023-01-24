fb7c7bd9c2e4a91e29d70711dbf0b244c0c13338
munevvernure <116505664+munevvernure@users.noreply.github.com>
2023-01-24 15:14:44 +0300

    Add files via upload

---
+++ valueOfEquation.cs
@@ -0,0 +1,22 @@
+﻿//Write a C# Sharp program to display certain values of the function x = y2 + 2y + 1
+//(using integer numbers for y , ranging from -5 to +5).
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cshharpDataTypeExcersises
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Type the value of y to calculate the value of the equation x = y^2 + 2y + 1");
+            Console.Write("Value of 'y' is :");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int x = (y * y) + (2 * y) + 1;
+            Console.WriteLine("Result of the given equation is:" + x);
+        }
+    }
+}
